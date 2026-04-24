d6bb6fe4f06354aa9e42f7522635ac898feb87fc
LeThiBaoUyen <142117332+LeThiBaoUyen@users.noreply.github.com>
2026-04-24 14:20:40 +0700

    food_service
    food_service

---
+++ mini_food_service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO foods (id, name, price, description, image_url) VALUES
+(101, 'Com Tam', 35000, 'Com tam suon bi cha', 'url_anh_1'),
+(102, 'Bun Bo', 45000, 'Bun bo Hue dac biet', 'url_anh_2');
